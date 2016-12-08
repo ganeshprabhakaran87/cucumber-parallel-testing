af6c147cef4a80943dea6cadc4ef13ad47f708d7
Stuart Wilson <stuart.r.wilson@gmail.com>
2016-12-08 19:08:09 +0000

    Correct renamed package in tests

--- cucumber-parallel-testing-acceptance/src/test/java/uk/co/hmtt/cucumber/parallel/bdd/runner/ThirdParallelTestRunner.java
+++ cucumber-parallel-testing-acceptance/src/test/java/uk/co/hmtt/cucumber/parallel/bdd/runner/ThirdParallelTestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import uk.co.hmtt.cucumber.parallel.runner.ParallelCucumber;
 
 @RunWith(ParallelCucumber.class)
-@CucumberOptions(glue = {"uk.co.hmtt.parallel.bdd.tests"},
+@CucumberOptions(glue = {"uk.co.hmtt.cucumber.parallel.bdd.tests"},
         format = {"pretty", "html:target/cucumber/cucumber-html-report-3", "json:target/cucumber/json/cucumber-3.json"},
         features = {"src/test/resources/features"},
         tags = {"~@wip"})
